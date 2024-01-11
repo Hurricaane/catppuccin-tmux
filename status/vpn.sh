814f2d9eeb8477cba11d28be1d297db1a007ecca
Yannick Dossou <yannick.dossou@octo.com>
2024-01-11 16:26:50 +0100

    status: add vpn module

---
+++ status/vpn.sh
@@ -0,0 +1,30 @@
+show_vpn() {
+  local vpn_name
+  local vpn_icon
+
+  vpn_name=$(get_vpn_name)
+  vpn_icon=""
+
+  # Module
+  local index
+  local icon
+  local color
+  local text
+  local module
+
+  index=$1
+  icon=$(get_tmux_option "@catppuccin_vpn_icon" "$vpn_icon")
+  color=$(get_tmux_option "@catppuccin_vpn_color" "$thm_magenta")
+  text=$(get_tmux_option "@catppuccin_vpn_text" "$vpn_name")
+  module=$(build_status_module "$index" "$icon" "$color" "$text")
+
+  echo "$module"
+}
+
+get_vpn_name() {
+  if command -v nmcli &>/dev/null; then
+    nmcli -f NAME,TYPE con show --active | grep --color=never -i "vpn" | awk '{print $1}'
+  else
+    echo "No Active VPN"
+  fi
+}
