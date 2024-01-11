3e6668d92a548ab41ec2ee4ec6fe7ee346dbc146
Yannick Dossou <yannick.dossou@octo.com>
2024-01-11 15:47:19 +0100

    status: modify battery module

--- status/battery.sh
+++ status/battery.sh
@@ -1,23 +1,62 @@
 show_battery() {
-  tmux set-option -g @batt_icon_charge_tier8 '󰁹'
-  tmux set-option -g @batt_icon_charge_tier7 '󰂁'
-  tmux set-option -g @batt_icon_charge_tier6 '󰁿'
-  tmux set-option -g @batt_icon_charge_tier5 '󰁾'
-  tmux set-option -g @batt_icon_charge_tier4 '󰁽'
-  tmux set-option -g @batt_icon_charge_tier3 '󰁼'
-  tmux set-option -g @batt_icon_charge_tier2 '󰁻'
-  tmux set-option -g @batt_icon_charge_tier1 '󰁺'
-  tmux set-option -g @batt_icon_status_charged '󰚥'
-  tmux set-option -g @batt_icon_status_charging '󰂄'
-  tmux set-option -g @batt_icon_status_discharging '󰂃'
-  tmux set-option -g @batt_icon_status_unknown '󰂑'
+  local battery_icon
+  local battery_percentage
 
-  local index=$1
-  local icon=$(get_tmux_option "@catppuccin_battery_icon" "#{battery_icon}")
-  local color=$(get_tmux_option "@catppuccin_battery_color" "$thm_yellow")
-  local text=$(get_tmux_option "@catppuccin_battery_text" "#{battery_percentage}")
+  battery=$(get_battery_percentage)
+  battery_icon=$(display_icon "$battery")
+  battery_percentage="${battery} %"
 
-  local module=$( build_status_module "$index" "$icon" "$color" "$text" )
+  # Module
+  local index
+  local icon
+  local color
+  local text
+  local module
+
+  index=$1
+  icon=$(get_tmux_option "@catppuccin_battery_icon" "$battery_icon")
+  color=$(get_tmux_option "@catppuccin_battery_icon" "$thm_red")
+  text=$(get_tmux_option "@catppuccin_battery_icon" "$battery_percentage")
+  module=$(build_status_module "$index" "$icon" "$color" "$text")
 
   echo "$module"
+}
+
+get_battery_percentage() {
+  if command -v upower &>/dev/null; then
+    upower -i "$(upower -e | grep BAT)" | grep --color=never -E "percentage" | awk '{print $2}' | tr -d '%'
+  elif command -v wsl &>/dev/null; then
+    bat_percentage=$(wsl powershell -command "& {Get-WmiObject -Class 'BatteryStatus' -Namespace 'root/cimv2/power' | Select-Object -ExpandProperty 'EstimatedChargeRemaining'}")
+    echo "$bat_percentage"
+  else
+    echo "Battery Status Unknown"
+    exit 1
+  fi
+}
+
+display_icon() {
+  percentage=$1
+  state=$(upower -i "$(upower -e | grep BAT)" | grep --color=never -E "state" | awk '{print $2}')
+
+  if [ "$state" = "charging" ]; then
+    echo "󰚥"
+  elif [ "$percentage" -ge 90 ]; then
+    echo "󰁹"
+  elif [ "$percentage" -ge 80 ]; then
+    echo "󰁹"
+  elif [ "$percentage" -ge 70 ]; then
+    echo "󰂁"
+  elif [ "$percentage" -ge 60 ]; then
+    echo "󰁿"
+  elif [ "$percentage" -ge 50 ]; then
+    echo "󰁾"
+  elif [ "$percentage" -ge 40 ]; then
+    echo "󰁽"
+  elif [ "$percentage" -ge 30 ]; then
+    echo "󰁼"
+  elif [ "$percentage" -ge 20 ]; then
+    echo "󰁻"
+  elif [ "$percentage" -ge 10 ]; then
+    echo "󰁺"
+  fi
 }
